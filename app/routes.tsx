66ddf5d9eb5ab5f70f8eae217b9e7d799730b17a
Masoud Ghorbani <msud.ghorbani@gmail.com>
2018-06-04 00:59:47 +0430

    add login route

--- app/routes.tsx
+++ app/routes.tsx
@@ -2,10 +2,12 @@ import * as React from 'react'
 import { Switch, Route } from 'react-router'
 import App from './containers/App'
 import MainWindow from './containers/MainWindow'
+import LoginWindow from './containers/LoginWindow'
 
 export default () => (
   <App>
     <Switch>
+      <Route path='/login' component={LoginWindow} />
       <Route path='/' component={MainWindow} />
     </Switch>
   </App>
