49604e9e7ed532745f452c8a217ce7fdd8d2823f
Masoud Ghorbani <msud.ghorbani@gmail.com>
2018-06-06 03:59:39 +0430

    rewrite constant module

---
+++ app/modules/constants.ts
@@ -0,0 +1,11 @@
+// is-electron-renderer doesn't have type defination
+const isRenderer = require('is-electron-renderer')
+import { app, remote } from 'electron'
+import { join } from 'path'
+
+let currentApp = isRenderer ? remote.app : app
+
+export const constants = {
+  profilePath: join(currentApp.getPath('userData'), 'phizog.json'),
+  profileTemplatePath: join(__dirname, '..', 'resources', 'phizog.json')
+}
