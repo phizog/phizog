52b05d7d4ddaa6a7fb5a6718ec1af747c0266ead
Masoud Ghorbani <msud.ghorbani@gmail.com>
2018-06-06 21:15:03 +0430

    remove useless pattern

--- scripts/test-runner.js
+++ scripts/test-runner.js
@@ -2,10 +2,8 @@ const spawn = require('cross-spawn')
 const path = require('path')
 
 // regex pattern to extract test files
-const pattern = `^(?!.*.d.tsx?$).*.[tj]sx?$`
 const result = spawn.sync(
   path.normalize('./node_modules/jest/bin/jest.js'),
-  [pattern],
   { stdio: 'inherit' }
 )
 
