5a93dd881c76004769a6a424c5b7aaacc7d54045
Masoud Ghorbani <msud.ghorbani@gmail.com>
2018-07-02 01:05:58 +0430

    set main window size

--- app/components/Home/index.tsx
+++ app/components/Home/index.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react'
 import { authorizer } from '../../providers/auth'
+import { remote } from 'electron'
+import { constants } from '../../modules/constants'
 
 @authorizer()
 export default class Home extends React.Component<any> {
+  componentWillMount () {
+    let win = remote.getCurrentWindow()
+    win.setResizable(true)
+    win.setMinimumSize(
+      constants.windows.main.width,
+      constants.windows.main.height
+    )
+    win.setSize(constants.windows.main.width, constants.windows.main.height)
+    win.center()
+  }
   render () {
     return (
       <div>
