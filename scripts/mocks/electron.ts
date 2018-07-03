c8ff5f8f67a87aa58b39a30fb90e1d17ea39454e
Masoud Ghorbani <msud.ghorbani@gmail.com>
2018-07-03 14:16:39 +0430

    mock electron functions

--- scripts/mocks/electron.ts
+++ scripts/mocks/electron.ts
@@ -2,5 +2,11 @@
 export const remote = {
   app: {
     getPath: jest.fn().mockReturnValue('path')
-  }
+  },
+  getCurrentWindow: jest.fn().mockReturnValue({
+    setResizable: jest.fn(),
+    setMinimumSize: jest.fn(),
+    setSize: jest.fn(),
+    center: jest.fn()
+  })
 }
