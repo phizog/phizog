0ed778c8d0e1ebfdf16f5a04040b595a0224c02d
Masoud Ghorbani <msud.ghorbani@gmail.com>
2018-07-11 23:34:06 +0430

    lint tests

--- app/components/home/home.spec.tsx
+++ app/components/home/home.spec.tsx
@@ -7,7 +7,17 @@ describe('<Home />', () => {
   let mock: any = jest.fn()
 
   it('should render correctly', () => {
-    const component = shallow(<Home inProgress={true} toggle={mock} profile={mock} history={mock} location={mock} match={mock} staticContext={mock} />)
+    const component = shallow(
+      <Home
+        inProgress={true}
+        toggle={mock}
+        profile={mock}
+        history={mock}
+        location={mock}
+        match={mock}
+        staticContext={mock}
+      />
+    )
 
     expect(component).toMatchSnapshot()
   })
--- app/components/login/login.spec.tsx
+++ app/components/login/login.spec.tsx
@@ -7,7 +7,17 @@ describe('<Login />', () => {
   let mock: any = jest.fn()
 
   it('should render correctly', () => {
-    const component = shallow(<Login inProgress={true} toggle={mock} profile={mock} history={mock} location={mock} match={mock} staticContext={mock} />)
+    const component = shallow(
+      <Login
+        inProgress={true}
+        toggle={mock}
+        profile={mock}
+        history={mock}
+        location={mock}
+        match={mock}
+        staticContext={mock}
+      />
+    )
 
     expect(component).toMatchSnapshot()
   })
