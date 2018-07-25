3d93e6da6237e1f20f51f77bc949e82d483d5a01
Masoud Ghorbani <msud.ghorbani@gmail.com>
2018-07-25 18:21:57 +0430

    set titlebar height and format control buttons according to os

--- app/components/titlebar/index.tsx
+++ app/components/titlebar/index.tsx
@@ -4,35 +4,40 @@ import { Row, Col } from 'react-flexbox-grid'
 import * as classnames from 'classnames'
 import variables from '../css/variables'
 import { remote } from 'electron'
+import * as is from 'electron-is'
 
 const TitlebarStyle = styled.div`
+  -webkit-app-region: drag;
   height: 36px;
-  background: ${variables.backColorPrimary} .logo {
-    img {
-      width: 32px;
-    }
+  background: ${variables.backColorPrimary};
+`
+
+const Logo = styled.div`
+  img {
+    width: 32px;
+    height: 36px;
   }
 `
 
 const Controls = styled.div``
+const Nav = styled.div``
 
 export interface IState {
   window: {
     maximized?: boolean
-    minimized?: boolean
   }
 }
 
 let win = remote.getCurrentWindow()
-
 export default class Titlebar extends React.Component<any, IState> {
   constructor (props: any) {
     super(props)
     this.maximizeWindow = this.maximizeWindow.bind(this)
+    this.minimizeWindow = this.minimizeWindow.bind(this)
+    this.closeWindow = this.closeWindow.bind(this)
     this.state = {
       window: {
-        maximized: false,
-        minimized: false
+        maximized: false
       }
     }
   }
@@ -46,6 +51,12 @@ export default class Titlebar extends React.Component<any, IState> {
     if (win.isMaximized()) return win.unmaximize()
     return win.maximize()
   }
+  closeWindow () {
+    return win.close()
+  }
+  minimizeWindow () {
+    return win.minimize()
+  }
   componentWillMount () {
     this.setState({
       window: {
@@ -54,23 +65,61 @@ export default class Titlebar extends React.Component<any, IState> {
     })
   }
   render () {
+    let controlButtons = [
+      {
+        label: 'Close',
+        function: this.closeWindow
+      },
+      {
+        label: 'Minimize',
+        function: this.minimizeWindow
+      },
+      {
+        label: 'Maximize',
+        function: this.maximizeWindow
+      }
+    ]
+
+    // change buttons order in case of Windows OS
+    if (is.windows()) {
+      controlButtons.splice(1, 0, ...controlButtons.splice(2, 1))
+      controlButtons.reverse()
+    }
+
+    const controlsList = controlButtons.map((item, index) => {
+      return (
+        <button onClick={item.function} key={index}>
+          {item.label}
+        </button>
+      )
+    })
+
     return (
       <TitlebarStyle>
-        <Row middle='xs'>
+        <Row
+          middle='xs'
+          style={{ flexDirection: is.windows() ? 'row-reverse' : 'row' }}
+        >
           <Col xs={4}>
-            <Controls className={classnames('align_left')}>
-              <button onClick={this.maximizeWindow}>
-                {this.state.window.maximized ? 'unmaximize' : 'maximize'}
-              </button>
+            <Controls
+              className={classnames(
+                is.windows() ? 'align_right' : 'align_left'
+              )}
+            >
+              {controlsList}
             </Controls>
           </Col>
           <Col xs={4}>
-            <div className={classnames('align_center', 'logo')}>
+            <Logo className={classnames('align_center')}>
               <img src='resources/logo/phizog.svg' />
-            </div>
+            </Logo>
           </Col>
           <Col xs={4}>
-            <div className={classnames('align_right', 'nav')}>a</div>
+            <Nav
+              className={classnames(
+                is.windows() ? 'align_left' : 'align_right'
+              )}
+            />
           </Col>
         </Row>
       </TitlebarStyle>
