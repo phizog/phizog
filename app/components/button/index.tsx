ecf430a4a1e391ba061df480b138d9d0cf9b4ae4
Masoud Ghorbani <msud.ghorbani@gmail.com>
2018-07-28 18:19:59 +0430

    Create button component

---
+++ app/components/button/index.tsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+import variables from '../css/variables'
+
+export const Button = styled.button`
+  padding: 2px 9px 4px 7px
+  border: 1px solid transparent;
+  border-color: #c2c0c2 #c2c0c2 #a19fa1;
+  color: ${variables.buttonColor}
+  border-radius: ${variables.borderRadius}
+  background: linear-gradient(to top, #f1f1f1, #fcfcfc);
+  box-shadow: 0 1px 1px rgba(0,0,0,.06);
+  outline: 0
+  svg {
+    vertical-align: middle
+  }
+  span {
+    vertical-align: middle
+    font-size: .813em
+    margin-left: 3px
+    font-weight: 500
+  }
+  &:hover {
+    background: #ddd
+  }
+  &:active {
+    background: #6d6c6d
+    border-color: #6d6c6d
+    color: #fff
+    box-shadow: 0
+    svg {
+      fill: #fff
+    }
+  }
+  &.transparent {
+    background: none
+    border: 0
+    box-shadow: none;
+    &:hover {
+      color: #fff
+      svg {
+        fill: #fff
+      }
+    }
+  }
+`
+
+export const GroupButton = styled.div`
+  button {
+    border-radius: 0
+    &:first-child {
+      border-radius: ${variables.borderRadius} 0 0 ${variables.borderRadius};
+    }
+    &:last-child {
+      border-radius: 0 ${variables.borderRadius} ${variables.borderRadius} 0;
+    }
+  }
+`
--- app/components/css/variables.ts
+++ app/components/css/variables.ts
@@ -2,5 +2,7 @@ export default {
   systemFonts: `Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica,
   Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
   textColorPrimary: '#f1f1f1',
-  backColorPrimary: '#141414'
+  backColorPrimary: '#141414',
+  buttonColor: '#444',
+  borderRadius: '3px'
 }
